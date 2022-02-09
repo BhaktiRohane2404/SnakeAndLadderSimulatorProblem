c6c66c392f2a75804fc1309106adf0401bc04430
BhaktiRohane2404 <98884739+BhaktiRohane2404@users.noreply.github.com>
2022-02-09 07:10:57 -0800

    Add files via upload

---
+++ SnakeandLadderSimulatorUC1.java
@@ -0,0 +1,8 @@
+package com.SnakeandLadder;
+
+public class SnakeandLadderSimulatorUC1 {
+    public static void main(String[] args) {
+        int START_POSITION = 0;
+        System.out.println("Player start at position " +START_POSITION );
+    }
+}
